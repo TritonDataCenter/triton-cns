7c179811996e2601abcad49c6f84a0602772ee57
Alex Wilson <alex.wilson@joyent.com>
2015-12-28 15:59:24 -0800

    CNS-56 reverse records should use alias name for VM

---
+++ test/unit/vm-to-zones.test.js
@@ -0,0 +1,190 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2015, Joyent, Inc.
+ */
+
+var test = require('./test-namer')('vm-to-zones');
+var util = require('util');
+var bunyan = require('bunyan');
+
+var utils = require('../../lib/utils');
+
+var buildZonesFromVm = require('../../lib/vm-to-zones');
+
+var log = bunyan.createLogger({name: 'cns'});
+
+test('basic single container', function (t) {
+	var config = {
+	    forward_zones: {
+		'foo': {}
+	    },
+	    reverse_zones: {}
+	};
+	var vm = {
+	    uuid: 'abc123',
+	    services: [],
+	    operation: 'add',
+	    owner: {
+		uuid: 'def432'
+	    },
+	    nics: [
+		{
+		    ip: '1.2.3.4',
+		    zones: ['foo']
+		}
+	    ]
+	};
+	var zones = buildZonesFromVm(vm, config, log);
+	t.deepEqual(Object.keys(zones), ['foo', '3.2.1.in-addr.arpa']);
+
+	t.deepEqual(Object.keys(zones['foo']), ['abc123.inst.def432']);
+	t.deepEqual(Object.keys(zones['3.2.1.in-addr.arpa']), ['4']);
+
+	var fwd = zones['foo']['abc123.inst.def432'];
+	t.deepEqual(fwd, [
+	    {constructor: 'A', args: ['1.2.3.4']},
+	    {constructor: 'TXT', args: ['abc123']}
+	]);
+	var rev = zones['3.2.1.in-addr.arpa']['4'];
+	t.deepEqual(rev, [
+	    {constructor: 'PTR', args: ['abc123.inst.def432.foo']}
+	]);
+
+	t.end();
+});
+
+test('with use_alias', function (t) {
+	var config = {
+	    use_alias: true,
+	    forward_zones: {
+		'foo': {}
+	    },
+	    reverse_zones: {}
+	};
+	var vm = {
+	    uuid: 'abc123',
+	    alias: 'test',
+	    services: [],
+	    operation: 'add',
+	    owner: {
+		uuid: 'def432'
+	    },
+	    nics: [
+		{
+		    ip: '1.2.3.4',
+		    zones: ['foo']
+		}
+	    ]
+	};
+	var zones = buildZonesFromVm(vm, config, log);
+	t.deepEqual(Object.keys(zones), ['foo', '3.2.1.in-addr.arpa']);
+
+	t.deepEqual(Object.keys(zones['foo']),
+	    ['abc123.inst.def432', 'test.inst.def432']);
+	t.deepEqual(Object.keys(zones['3.2.1.in-addr.arpa']), ['4']);
+
+	var fwd = zones['foo']['test.inst.def432'];
+	t.deepEqual(fwd, [
+	    {constructor: 'A', args: ['1.2.3.4']},
+	    {constructor: 'TXT', args: ['abc123']}
+	]);
+	var rev = zones['3.2.1.in-addr.arpa']['4'];
+	t.deepEqual(rev, [
+	    {constructor: 'PTR', args: ['test.inst.def432.foo']}
+	]);
+
+	t.end();
+});
+
+test('with use_login', function (t) {
+	var config = {
+	    use_login: true,
+	    forward_zones: {
+		'foo': {}
+	    },
+	    reverse_zones: {}
+	};
+	var vm = {
+	    uuid: 'abc123',
+	    alias: 'test',
+	    services: [],
+	    operation: 'add',
+	    owner: {
+		uuid: 'def432',
+		login: 'bar'
+	    },
+	    nics: [
+		{
+		    ip: '1.2.3.4',
+		    zones: ['foo']
+		}
+	    ]
+	};
+	var zones = buildZonesFromVm(vm, config, log);
+	t.deepEqual(Object.keys(zones), ['foo', '3.2.1.in-addr.arpa']);
+
+	t.deepEqual(Object.keys(zones['foo']),
+	    ['abc123.inst.def432', 'abc123.inst.bar']);
+	t.deepEqual(Object.keys(zones['3.2.1.in-addr.arpa']), ['4']);
+
+	var fwd = zones['foo']['abc123.inst.bar'];
+	t.deepEqual(fwd, [
+	    {constructor: 'A', args: ['1.2.3.4']},
+	    {constructor: 'TXT', args: ['abc123']}
+	]);
+	var rev = zones['3.2.1.in-addr.arpa']['4'];
+	t.deepEqual(rev, [
+	    {constructor: 'PTR', args: ['abc123.inst.def432.foo']}
+	]);
+
+	t.end();
+});
+
+test('with use_alias and use_login', function (t) {
+	var config = {
+	    use_alias: true,
+	    use_login: true,
+	    forward_zones: {
+		'foo': {}
+	    },
+	    reverse_zones: {}
+	};
+	var vm = {
+	    uuid: 'abc123',
+	    alias: 'test',
+	    services: [],
+	    operation: 'add',
+	    owner: {
+		uuid: 'def432',
+		login: 'bar'
+	    },
+	    nics: [
+		{
+		    ip: '1.2.3.4',
+		    zones: ['foo']
+		}
+	    ]
+	};
+	var zones = buildZonesFromVm(vm, config, log);
+	t.deepEqual(Object.keys(zones), ['foo', '3.2.1.in-addr.arpa']);
+
+	t.deepEqual(Object.keys(zones['foo']),
+	    ['abc123.inst.def432', 'abc123.inst.bar', 'test.inst.def432',
+	     'test.inst.bar']);
+	t.deepEqual(Object.keys(zones['3.2.1.in-addr.arpa']), ['4']);
+
+	var fwd = zones['foo']['test.inst.bar'];
+	t.deepEqual(fwd, [
+	    {constructor: 'A', args: ['1.2.3.4']},
+	    {constructor: 'TXT', args: ['abc123']}
+	]);
+	var rev = zones['3.2.1.in-addr.arpa']['4'];
+	t.deepEqual(rev, [
+	    {constructor: 'PTR', args: ['test.inst.bar.foo']}
+	]);
+
+	t.end();
+});
