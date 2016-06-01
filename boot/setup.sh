8446fd23b62a73da952ac8a6693d9b91b2d5c4a8
Alex Wilson <alex.wilson@joyent.com>
2016-06-01 14:22:30 -0700

    CNS-143 want better detection of admin NIC Reviewed by: Richard Kiene <richard.kiene@joyent.com>

--- boot/setup.sh
+++ boot/setup.sh
@@ -56,6 +56,9 @@ CONFIG_AGENT_LOCAL_MANIFESTS_DIRS=$SVC_ROOT
 source /opt/smartdc/boot/lib/util.sh
 sdc_common_setup
 
+echo "Adding CNS user"
+useradd -d /opt/triton/cns -P 'Metadata Reader' cns
+
 echo "Installing cns redis"
 sdc_setup_redis
 
