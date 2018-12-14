d75aa32ebbba9cf8d655a17631aa9a5e00a341b2
Alex Wilson <alex.wilson@joyent.com>
2018-12-14 10:52:14 -0800

    TRITON-1044 cns should use newer redis server and client, paginate AXFRs Reviewed by: Robert Mustacchi <rm@joyent.com> Reviewed by: Trent Mick <trent.mick@joyent.com> Approved by: Isaac Davis <isaac.davis@joyent.com>

--- updater.js
+++ updater.js
@@ -3,10 +3,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * Copyright (c) 2015, Joyent, Inc.
+ * Copyright (c) 2018, Joyent, Inc.
  */
 
-var redis = require('redis');
+var redis = require('ioredis');
 var bunyan = require('bunyan');
 var cueball = require('cueball');
 var util = require('util');
